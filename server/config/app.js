c77b0d5b49d4d30148c9f9595b41e7871c49afb1
Manthan Shah <manthanshah1711@gmail.com>
2019-02-16 10:17:08 -0800

    updated password

--- server/config/app.js
+++ server/config/app.js
@@ -1,8 +1,16 @@
+//modules for web server
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+
+//modules for authentication
+let session = require ('express-session');
+let passport = require ('passport');
+let passportLocal = require ('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect flash');
 
 // database setup
 let mongoose = require('mongoose');
@@ -34,8 +42,35 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+//setup session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: true,
+  resave: true
+}));
+
+//initialize passport and flash
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
+
+//passport user configuration
+
+//create user model
+
+let UserModel= require('../models/user');
+let User = UserModel.User;
+
+//implement user strategy
+passport.use(User.createStrategy());
+
+//serialize and deserialize
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
